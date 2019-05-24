dccae76ae3f2e2e5fdca495986e0c2dec1f31268
Facundo Palombo <facundo.palombo@gmail.com>
2019-05-24 15:22:51 -0300

    Pruebas de excepciones de snapshots

--- __test__/snapshot.test.ts
+++ __test__/snapshot.test.ts
@@ -6,4 +6,21 @@ describe('Prueba de los snapshots', ()=> {
     test('Snapshot', ()=> {
         expect(getCharacter(rick)).toMatchSnapshot()
     })
+    test('Siempre va a fallar la instantanea', ()=> {
+        const user = {
+            createdAt : new Date(),
+            id : Math.floor(Math.random() * 20),
+            name: "Random guy"
+        }
+        expect(user).toMatchSnapshot()
+    })
+    test('Tenemos una excepción dentro del código', ()=> {
+        const user = {
+            id : Math.floor(Math.random() * 20),
+            name: "Random guy"
+        }
+        expect(user).toMatchSnapshot({
+            id: expect.any(Number)
+        })
+    })
 })
